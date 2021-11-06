cf42bc12ee498119fdaae51640135800f6eb16ce
devmehta787 <dev.mehta2019@vitstudent.ac.in>
2021-11-07 03:38:38 +0530

    java script

--- game.js
+++ game.js
@@ -1,15 +1,48 @@
 var buttonColors=["red", "blue", "green", "yellow"], gamePattern=[], userClickedPattern=[];
 
+var level=0;
+var started = false;
+
+$(document).keypress(function(){
+    if (!started){
+        $("#level-title").text("Level "+level);
+        nextSequence();
+        started=true;
+    }
+});
+
+
 $(".btn").click(function(){
     var userChoosenColor= $(this).attr("id");
     userClickedPattern.push(userChoosenColor);
     
     playSound(userChoosenColor);
     animatePress(userChoosenColor);
+
+    checkAnswer(userClickedPattern.length-1);
 });
 
 
+
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+        console.log("success");
+        if (userClickedPattern.length===gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    }
+    else{
+        console.log("fail");
+    }
+}
+
 function nextSequence(){
+
+    userClickedPattern=[];
+    level++;
+    $("level-title").text("Level "+ level);
     var randomNumber = Math.floor((Math.random())*4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
